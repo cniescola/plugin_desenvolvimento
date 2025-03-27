ddf30e19cd59df362e7f4497607fd7b6504608ce
Assis-123 <joaopedrodeassis938@gmail.com>
2025-03-27 18:32:08 -0300

    Continuação da construção do layout Home

---
+++ Sistemjoao/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylehome.css">
+</head>
+<body>
+    <section>
+        <div id="background">
+            <div id="titulo">
+                <p>
+                    SISTEMA DE CADASTRO DE ALTERAÇÕES EM MASSA
+                </p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
